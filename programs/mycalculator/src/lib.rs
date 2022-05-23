d8afb8465c0dda71cf7363fbac68559ad8da2b4f
root <root@DESKTOP-87DPNVK.localdomain>
2022-05-23 02:38:25 +0200

    lib.rs endpoints

--- programs/mycalculator/src/lib.rs
+++ programs/mycalculator/src/lib.rs
@@ -6,10 +6,37 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mycalculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+        pub fn create(ctx:Context<Create>, init_message: String) -> Result<()> {
+            let calculator = &mut ctx.accounts.calculator ;
+            calculator.greeting = init_message ;
+
+            Ok(())
+
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> Result<()> {
+        
+            Ok(())
+    }
+
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> Result<()> {
+        
+            Ok(())
+    }
+
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> Result<()> {
+        
+            Ok(())
+    }
+
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> Result<()> {
+        
+            Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create {
+    #[account(mut)]
+   pub calculator: Accounts<T, Calculator>,
+}
